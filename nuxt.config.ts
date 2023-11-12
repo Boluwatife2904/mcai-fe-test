534605620399090d9339904c0becbc436ad35170
Boluwatife2904 <sanvicola2000@gmail.com>
2023-11-12 17:15:09 +0100

    chore: add meta tags to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			htmlAttrs: {
+				lang: "en",
+			},
+			title: "MyCoverAI Test",
+			meta: [
+				{ name: "theme-color", content: "#040415" },
+				{ name: "viewport", content: "width=device-width, initial-scale=1" },
+				{ name: "description", content: "Repository for the implementation of the task given for the frontend developer role at MyCoverAi " },
+				{ name: "og:title", content: "MyCoverAI Test" },
+				{ name: "og:site_name", content: "MyCoverAI Test" },
+				{ name: "og:url", content: "https://mcai-fe-test.vercel.app/" },
+				{ name: "og:description", content: "Repository for the implementation of the task given for the frontend developer role at MyCoverAi " },
+				{ name: "og:type", content: "website" },
+			],
+		},
+	},
 	devtools: { enabled: true },
 	css: ["@/assets/styles/fonts.scss", "@/assets/styles/index.scss"],
 	modules: ["@nuxtjs/tailwindcss", "nuxt-icons", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt"],
