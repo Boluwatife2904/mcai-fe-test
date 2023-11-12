0f09aa8812c28414aa7b84b7144ac8f4852d6961
Boluwatife2904 <sanvicola2000@gmail.com>
2023-11-12 14:21:06 +0100

    chore: update featured card increase test styles

--- components/dashboard/analytics/FeaturedCard.vue
+++ components/dashboard/analytics/FeaturedCard.vue
@@ -10,11 +10,11 @@ defineProps<Props>();
 </script>
 
 <template>
-	<div class="featured-card flex shrink-0 flex-col gap-[0.4rem] rounded-[0.8rem] px-[1.6rem] py-[0.8rem]" :class="[`featured-card--${variant}`]">
+	<div class="featured-card flex shrink-0 flex-col" :class="[`featured-card--${variant}`]">
 		<p class="text-[1.4rem] leading-[2rem] text-white">{{ name }}</p>
 		<div class="flex items-center gap-[2rem]">
 			<p class="text-[2rem] font-semibold leading-[3rem] text-white">N {{ value }}</p>
-			<span class="flex min-h-[1.7rem] min-w-[5.9rem] items-center gap-[0.3rem] rounded-[0.2rem] bg-primary-50 px-[0.3rem] py-[0.2rem] text-success-500">
+			<span class="featured-card__increase flex items-center bg-primary-50 text-[1.4rem] font-medium text-success-500">
 				<template v-if="increase">
 					<NuxtIcon name="trending-up" filled />
 					{{ increase }}
@@ -26,6 +26,8 @@ defineProps<Props>();
 
 <style lang="scss" scoped>
 .featured-card {
+	@apply gap-[0.4rem] rounded-[0.8rem] px-[1.6rem] py-[0.8rem];
+
 	&--mcai {
 		@apply bg-mcai;
 	}
@@ -36,6 +38,10 @@ defineProps<Props>();
 
 	&--yellow {
 		@apply bg-yellow;
+	}
+
+	&__increase {
+		@apply min-h-[1.7rem] min-w-[5.9rem]  gap-[0.3rem] rounded-[0.2rem] pl-[0.15rem] pr-[0.25rem] py-[0.1rem];
 	}
 }
 </style>
